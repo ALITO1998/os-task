15ee0ddbe53750b62aa567cd459774ba789d2876
ALITO1998 <34987442+ALITO1998@users.noreply.github.com>
2019-04-20 01:50:09 +0200

    last update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.uic import loadUi
+import subprocess as sbp
 
 class main(QDialog):
     def __init__(self):
@@ -13,7 +14,7 @@ class main(QDialog):
 
     def run(self):
         x = self.comboBox.currentText()
-        if (x == "ps"):
+        if (x == "List all the processes"):
             self.dialog = socand()
         if (x == "List all the processes grouped by user"):
             self.dialog = theard()
@@ -55,7 +56,13 @@ class fuorth(QDialog):
     def run(self):
         a = self.textbox.text()
         e = os.popen("pkill -9 "+a)
-        self.dialog = fuorth_r()
+        f = open(os.devnull,'w')
+        retcode = sbp.call(['pkill','-9',a],stdout=f,stderr=sbp.STDOUT)
+        if (retcode == 0):
+            self.dialog = fuorth_r()
+        else :
+            self.dialog = theard_r()
+        
         self.close()
         self.dialog.exec_()
 
@@ -89,29 +96,36 @@ class theard(QDialog):
         loadUi('test2_b.ui',self)
         self.goback.clicked.connect(self.go_back)
         self.Run.clicked.connect(self.run)
-        
-         
+        self.label_2.hide()
+        self.plainTextEdit.hide()
 
     def go_back(self):
         self.close()
 
     def run(self):
-        self.dialog = theard_r()
-        self.dialog.exec_()
-        self.close()
+        a = self.textbox.text()
+        w = os.popen("ps -u "+a).read()
+        self.Run.hide()
+        self.label.hide()
+        self.textbox.hide()
+        self.label_2.show()
+        self.plainTextEdit.show()
+        if (w == ''):
+            self.plainTextEdit.insertPlainText("error: user name does not exist")
+        else :
+            self.plainTextEdit.insertPlainText(w)
 
-class theard_r(theard):
-    def __init__(self, parent = theard):
+
+class theard_r(QDialog):
+    def __init__(self):
         super(theard_r,self).__init__()
         loadUi('test2_b_r.ui',self)
-        a = self.textbox.text()
-        q = os.popen("ps -u"+a).read()
-        self.plainTextEdit.insertPlainText(q)
         self.pushButton.clicked.connect(self.go_back)
 
 
     def go_back(self):
         self.close()
+
 
 class frist(QDialog):
     def __init__(self):
@@ -125,7 +139,13 @@ class frist(QDialog):
         a = self.spinBox.value()
         b = self.lineEdit.text()
         c = os.popen("pkill -"+str(a)+" "+b)
-        self.dialog = fuorth_r()
+        f = open(os.devnull,'w')
+        retcode = sbp.call(['pkill',str(a),a],stdout=f,stderr=sbp.STDOUT)
+        if (retcode == 0):
+            self.dialog = fuorth_r()
+        else :
+            self.dialog = theard_r()
+
         self.close()
         self.dialog.exec_()
     
